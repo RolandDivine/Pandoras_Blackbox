6c6e4e3f47e79e10b69a911934f91aa5e44200da
Roland Divine <45848152+RolandDivine@users.noreply.github.com>
2023-06-25 17:28:44 +0100

    Update Ownable.sol

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,1 +1,62 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Contract module that provides an ownable modifier and related functions.
+ *
+ * The `Ownable` contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract, setting the deployer as the initial owner.
+     */
+    constructor() {
+        _setOwner(msg.sender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Sets a new owner address.
+     */
+    function _setOwner(address newOwner) private {
+        _owner = newOwner;
+        emit OwnershipTransferred(address(0), newOwner);
+    }
+
+    /**
+     * @dev Internal function to transfer ownership of the contract to a new account (`newOwner`).
+     */
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _setOwner(newOwner);
+    }
+}
