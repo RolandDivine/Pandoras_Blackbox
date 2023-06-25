4abdf051935045d60ed9b8e6db920a838a14fa1e
Roland Divine <45848152+RolandDivine@users.noreply.github.com>
2023-06-25 17:21:31 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,1 +1,22 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network ID
+    },
+    // Add more network configurations as needed
+  },
+  compilers: {
+    solc: {
+      version: "0.8.12", // Specify the Solidity compiler version
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+};
 
