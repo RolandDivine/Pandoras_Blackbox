4708b74feddb3a03a23f19a0f1ba0c5e87e84969
Roland Divine <45848152+RolandDivine@users.noreply.github.com>
2023-06-25 17:35:58 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,13 @@
+const DarkPool = artifacts.require("DarkPool");
+
+module.exports = async function (deployer) {
+  // Deploy the DarkPool contract
+  await deployer.deploy(DarkPool);
+
+  // Get the deployed contract instance
+  const darkPool = await DarkPool.deployed();
+
+  // Log the contract address for reference
+  console.log("DarkPool contract deployed at:", darkPool.address);
+};
 
